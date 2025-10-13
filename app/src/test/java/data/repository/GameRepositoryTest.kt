52a183c4bf612f3440985bf7337d5ab7b4402feb
2025-DEV2-010 <ando.avenger@gmail.com>
2025-10-13 21:46:21 +0530

    test: add test case for invalid row path

--- app/src/test/java/data/repository/GameRepositoryTest.kt
+++ app/src/test/java/data/repository/GameRepositoryTest.kt
@@ -1,6 +1,7 @@
 package data.repository
 
 import com.kata.tictactoe.utils.generateWinningPaths
+import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
@@ -14,5 +15,15 @@ class GameRepositoryTest {
         val isValid = expectedData.contains(actualData)
 
         assertTrue(isValid)
+    }
+
+    @Test
+    fun `verify invalid row path`() {
+        val actualData = listOf(0, 1, 3)
+
+        val expectedData = generateWinningPaths()
+        val isValid = expectedData.contains(actualData)
+
+        assertFalse(isValid)
     }
 }
