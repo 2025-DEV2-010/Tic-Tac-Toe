62b3cfeb8845742eddd084847a9a9c32ed3cbe79
2025-DEV2-010 <ando.avenger@gmail.com>
2025-10-16 11:28:10 +0530

    refactor: make GameEngine for readable

--- app/src/main/java/com/kata/tictactoe/data/repository/GameEngine.kt
+++ app/src/main/java/com/kata/tictactoe/data/repository/GameEngine.kt
@@ -6,23 +6,38 @@ class GameEngine(private val boardSize: Int = 3) {
 
         val pathList = mutableListOf<List<Int>>()
 
-        for (row in 0 until boardSize) {
-            val rows = (0 until boardSize).map { column -> row * boardSize + column }
-            pathList.add(rows)
-        }
-
-        for (row in 0 until boardSize) {
-            val rows = (0 until boardSize).map { column -> column * boardSize + row }
-            pathList.add(rows)
-        }
-
-        val mainDiagonal = (0 until boardSize).map { item -> item * boardSize + item }
-        pathList.add(mainDiagonal)
-
-        val antiDiagonal =
-            (0 until boardSize).map { item -> item * boardSize + (boardSize - 1 - item) }
-        pathList.add(antiDiagonal)
+        pathList.addAll(getWinningRowItems())
+        pathList.addAll(getWinningColumnItems())
+        pathList.add(getMainDiagonalItem())
+        pathList.add(getAntiDiagonalItem())
 
         return pathList
     }
+
+    private fun getWinningRowItems(): MutableList<List<Int>> {
+        val rowList = mutableListOf<List<Int>>()
+        for (row in 0 until boardSize) {
+            val rows = (0 until boardSize).map { col -> row * boardSize + col }
+            rowList.add(rows)
+        }
+        return rowList
+    }
+
+    private fun getWinningColumnItems(): MutableList<List<Int>> {
+        val columnList = mutableListOf<List<Int>>()
+        for (row in 0 until boardSize) {
+            val rows = (0 until boardSize).map { col -> col * boardSize + row }
+            columnList.add(rows)
+        }
+        return columnList
+    }
+
+    private fun getMainDiagonalItem(): List<Int> {
+        return (0 until boardSize).map { item -> item * boardSize + item }
+    }
+
+    private fun getAntiDiagonalItem(): List<Int> {
+        return (0 until boardSize).map { item -> item * boardSize + (boardSize - 1 - item) }
+    }
+
 }
